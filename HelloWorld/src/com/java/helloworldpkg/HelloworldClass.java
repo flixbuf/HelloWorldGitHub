ae2ed28e5f48a513fbc6e90d9e1b383fcdc44f90
Flix Buf <flixbuf@gmail.com>
2016-02-05 10:30:51 -0800

    A greeting is checked in

--- HelloWorld/src/com/java/helloworldpkg/HelloworldClass.java
+++ HelloWorld/src/com/java/helloworldpkg/HelloworldClass.java
@@ -4,7 +4,9 @@ public class HelloworldClass {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World!");
+		System.out.println("Hello Mayur!");
+		System.out.print(" How are you doing");
+		System.out.println();
 	}
 
 }
