22fa3c704edb64e3db1a7887ad18db862afa43a0
Flix Buf <flixbuf@gmail.com>
2016-02-05 10:36:54 -0800

    Removed new line

--- HelloWorld/src/com/java/helloworldpkg/HelloworldClass.java
+++ HelloWorld/src/com/java/helloworldpkg/HelloworldClass.java
@@ -4,7 +4,7 @@ public class HelloworldClass {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello Mayur!");
+		System.out.print("Hello Mayur!");
 		System.out.print(" How are you doing");
 		System.out.println();
 	}
